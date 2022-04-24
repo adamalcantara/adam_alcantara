debcf14e5751a647855d532eed54f1476bcd1ca0
adamalcantara <alcantaradevelopment@gmail.com>
2022-04-24 08:40:18 -0400

    Card imported, working properly

--- src/Pages/LandingPage/LandingPage.js
+++ src/Pages/LandingPage/LandingPage.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
 
+// import card
+import Card from '../../Components/Card/Card';
 
 const LandingPage = () => {
     
   return (
     <div>
         <h1>Adam Alcantara</h1>
+        <Card />
     </div>
   )
 }
